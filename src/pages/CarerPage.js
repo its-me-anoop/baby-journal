22508adc34526b9a405768d4f07effd85fd7d33c
Anoop Jose <anoop@flutterly.co.uk>
2024-07-26 11:02:15 +0100

    feat: Add CarerPage component
    The commit adds the CarerPage component to the src/pages directory. The CarerPage component is responsible for rendering the CarerDashboard component, which displays the dashboard for carers. This feature enhances the functionality of the application by providing a dedicated page for carers to view their dashboard.

---
+++ src/pages/CarerPage.js
@@ -0,0 +1,13 @@
+// src/pages/CarerPage.js
+import React from 'react';
+import CarerDashboard from '../components/CarerDashboard';
+
+const CarerPage = () => {
+    return (
+        <div>
+            <CarerDashboard />
+        </div>
+    );
+};
+
+export default CarerPage;
