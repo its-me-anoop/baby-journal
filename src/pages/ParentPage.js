8eaf2751ffc75a1ec7c3dd6f7025548e4591fc6b
Anoop Jose <anoop@flutterly.co.uk>
2024-07-26 11:00:58 +0100

    feat: Add ParentPage component
    This commit adds the ParentPage component to the src/pages directory. The ParentPage component is responsible for rendering the ParentDashboard component, which displays journal entries for parents. This feature enhances the functionality of the application by providing a dedicated page for parents to view their children's journal entries.

---
+++ src/pages/ParentPage.js
@@ -0,0 +1,13 @@
+// src/pages/ParentPage.js
+import React from 'react';
+import ParentDashboard from '../components/ParentDashboard';
+
+const ParentPage = () => {
+    return (
+        <div>
+            <ParentDashboard />
+        </div>
+    );
+};
+
+export default ParentPage;
